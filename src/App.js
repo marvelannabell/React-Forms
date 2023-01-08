9b0b1752ef3d5fca1b241e484b5c11110cc6c37a
Vel MAr <velmar.annabell@gmail.com>
2023-01-08 23:21:30 +0200

    controlled-forms

--- src/App.js
+++ src/App.js
@@ -6,34 +6,112 @@ import { useState } from 'react';
 
 
 function App() {
-  const [userName,setUsername]= useState('defaultValue');
-  console.log(userName);//we cat use current userName after change
-const submitHandler=(e)=>{
-  e.preventDefault()
-}
+  const [values, setValues] = useState({
+    username: '',
+  password: '',
+age:'',
+bio:'',
+gender: 'm',
+userType:'corporate',
+tac: false
 
-const usernameChangeHandler = (e)=>{
-  e.preventDefault();
-  setUsername(e.currentTarget.value);
-}
+
+});
+  const [bio, setBio] = useState('');
+  const [gender, setGender] = useState('');
+  const [userType, setUserType] = useState('corporate')
+  const [tac, setTac] = useState(true)
+  
+  
+  
+  
+  
+
+  //console.log(userName);//we cat use current userName after change
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    // let values = Object.fromEntries(new FormData(e.target));
+    // console.log("pass", password);
+    // console.log("name", userName);
+    // console.log("age", age);
+    // console.log("gender", gender);
+    // console.log("userType",userType);
+    console.log(values);
+  }
+  
+  const changeHandler =(e)=>{
+    console.log(e.target.name,'-',e.target.value);
+    
+    setValues(oldState=>({
+      ...oldState,
+      [e.target.name]:e.target.type =="checkbox"? e.target.checked : e.target.value
+    }))
+  };
+
+  // const checkboxHandler =(e)=>{
+  //   // console.log(e.target.name,'-',e.target.checked);
+    
+  //   setValues(oldState=>({
+  //     ...oldState,
+  //     [e.target.name]:e.target.checked
+  //   }))
+  // }
+
+  // const usernameChangeHandler = (e) => {
+  //   e.preventDefault();
+  //   changeHandler(e.currentTarget.value);
+ 
+  // }
   return (
     <div className="App">
       <header className="App-header">
         <form onSubmit={submitHandler} >
           <div>
-            <label htmlFor="username">Username</label>
-            <input 
-            id='username' 
-            type="text" 
-            name="username" 
-            onChange={usernameChangeHandler}
-            value={userName}/>
-            
+            <label htmlFor="username">Username: </label>
+            <input
+              id='username'
+              type="text"
+              name="username"
+              onChange={changeHandler}
+              value={values.username} />
+
           </div>
           <div>
-            <label htmlFor="passwors">Password</label>
-            <input id='password' type="text" anme="password" />
+            <label htmlFor="password">Password: </label>
+            <input id='password' type="password" name="password" value={values.password} onChange={changeHandler}/>
           </div>
+
+          <div>
+            <label htmlFor="age">Age: </label>
+            <input id='age' type="number" name="age" value={values.age} onChange={changeHandler} />
+          </div>
+
+          <div>
+            <label htmlFor="bio">Bio: </label>
+            <textarea name="bio" id="bio" cols="30" rows="10" value={values.bio} onChange={changeHandler}></textarea>
+          </div>
+
+          <div>
+            <label htmlFor="gender" >Gender: </label>
+            <select name="gender" id="gender" value={values.gender} onChange={changeHandler}>
+              <option value="m">Male</option>
+              <option value="f">Female</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="tac">Terms and Conditions: </label>
+            <input type="checkbox" name='tac' id="tac"  onChange={changeHandler}/>
+          </div>
+          <div>
+          <label htmlFor="individual-user-type">Individual: </label>
+          <input type="radio" name='userType' value="individual" id="individual-user-type" onChange={changeHandler} checked={values.userType==="individual"}/>
+          <label htmlFor="corporate-user-type">Corporate: </label>
+          <input type="radio" name='userType' value="corporate" id="corporate-user-type" onChange={changeHandler} checked={values.userType==="corporate"}/>
+
+          </div>
+
           <div>
             <input type="submit" value='Login' />
           </div>
